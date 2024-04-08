8261e3bca0c55a84c9f34f07de63d076d2c840e2
iscyy <wenvoi@163.com>
2024-04-08 11:34:56 +0800

    update

--- models/yolo.py
+++ models/yolo.py
@@ -14,7 +14,7 @@ from utils.torch_utils import time_synchronized, fuse_conv_and_bn, model_info, s
     select_device, copy_attr
 from utils.loss import SigmoidBin
 
-from models.CoreV7.EMO import C3_RMB, CSRMBC, C2f_RMB, CPNRMB, ReNLANRMB
+# from models.CoreV7.EMO import C3_RMB, CSRMBC, C2f_RMB, CPNRMB, ReNLANRMB
 from models.CoreV7.Dysample import DySample
 from .gelan import RepNCSPELAN4, SPPELAN
 
@@ -790,14 +790,6 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
                 args.insert(2, n)  # number of repeats
                 n = 1
         # 新增模块
-        elif m in [C3_RMB, CSRMBC, C2f_RMB, CPNRMB, ReNLANRMB]:
-            c1, c2 = ch[f], args[0]
-            if c2 != no:  # if not output
-                c2 = make_divisible(c2 * gw, 8)
-            args = [c1, c2, *args[1:]]
-            if m in [C3_RMB, CSRMBC, C2f_RMB, CPNRMB]:
-                args.insert(2, n)  # number of repeats
-                n = 1
         elif m in [DySample]:
             args = [ch[f], *args[0:]]
         elif m in [C3GhostV2]:
